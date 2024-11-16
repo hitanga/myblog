a8c1f283fbc88f0817c4cdd7748c15eae2043749
hitanga <hitanga@gmail.com>
2024-11-16 20:05:54 +0530

    reactive forms updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
